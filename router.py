9be280855c4af9c5f1ed92f16d84d3dac252f2bc
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-15 22:09:13 +0100

    validate model 177 - ep 48

--- router.py
+++ router.py
@@ -14,8 +14,8 @@ trans = transforms.Compose([transforms.ToTensor()])
 ENCODER_NAME = "resnet34"
 
 # PATH = "network/models_save/CV-173/MODEL-Unetresnet34CV-173_EPOCH_76.pth"
-PATH = "network/models_save/CV-176/MODEL-Unetresnet34CV-176_EPOCH_83.pth"
-# PATH = "network/models_save/CV-177/MODEL-Unetresnet34CV-177_EPOCH_48.pth"
+# PATH = "network/models_save/CV-176/MODEL-Unetresnet34CV-176_EPOCH_83.pth"
+PATH = "network/models_save/CV-177/MODEL-Unetresnet34CV-177_EPOCH_48.pth"
 
 
 # Create the model
