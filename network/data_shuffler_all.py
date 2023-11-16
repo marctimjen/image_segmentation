35953e76bc60d23b4518d734a35a4a26a0762e7f
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-16 11:57:46 +0100

    do the entire data-shuffle

---
+++ network/data_shuffler_all.py
@@ -0,0 +1,93 @@
+# This file is used for taking the data from the dataset and shuffle and append it in a
+# train and validation set.
+
+import os
+import numpy as np
+import shutil
+
+data_path = rf"/datasets/tumor_segmentation_pics_from_repo"
+
+split = 0.15  # how many pct. for val.
+
+np.random.seed(42)
+print("Remember to change seed if necessary")
+
+files = os.listdir(data_path)
+
+start_path = "image_data_all"
+if os.path.exists(start_path):
+    shutil.rmtree(start_path)
+os.makedirs(start_path)
+
+
+train_path = "image_data_all/train"
+os.makedirs(train_path)
+
+train_path = "image_data_all/train/images"
+os.makedirs(train_path)
+
+train_path = "image_data_all/train/masks"
+os.makedirs(train_path)
+
+
+val_path = "image_data_all/val"
+os.makedirs(val_path)
+
+val_path = "image_data_all/val/images"
+os.makedirs(val_path)
+
+val_path = "image_data_all/val/masks"
+os.makedirs(val_path)
+
+
+control_list = [f for f in files if "control" in f]
+control_val = np.random.choice(control_list, size=int(split * len(control_list)), replace=False)
+
+val_path = "image_data_all/val"
+train_path = "image_data_all/train"
+for file in control_list:
+    source_path_img = data_path + "/" + file
+    source_path_mask = data_path + "/target_seg_" + file[-7:]
+    if file in control_val:
+        shutil.copy(source_path_img, val_path + "/images")
+        shutil.copy(source_path_mask, val_path + "/masks")
+    else:
+        shutil.copy(source_path_img, train_path + "/images")
+        shutil.copy(source_path_mask, train_path + "/masks")
+
+
+patient_list = [f for f in files if "patient" in f]
+patient_val = np.random.choice(patient_list, size=int(split * len(patient_list)), replace=False)
+
+for file in patient_list:
+    source_path_img = data_path + "/" + file
+    source_path_mask = data_path + "/segmentation_" + file[-7:]
+    if file in patient_val:
+        shutil.copy(source_path_img, val_path + "/images")
+        shutil.copy(source_path_mask, val_path + "/masks")
+    else:
+        shutil.copy(source_path_img, train_path + "/images")
+        shutil.copy(source_path_mask, train_path + "/masks")
+
+
+# after the split of their data, we can move the rest of the files into training:
+
+data_path = rf"/datasets/tumor_segmentation_mnm_anno"
+rest_files = os.listdir(data_path)
+
+
+patient_list = [f for f in rest_files if "patient" in f]
+for file in patient_list:
+    source_path_img = data_path + "/" + file
+    source_path_mask = data_path + "/segmentation_" + file[-7:]
+    shutil.copy(source_path_img, train_path + "/images")
+    shutil.copy(source_path_mask, train_path + "/masks")
+
+
+control_list = [f for f in rest_files if "control" in f]
+for file in control_list:
+    source_path_img = data_path + "/" + file
+    source_path_mask = data_path + "/target_seg_" + file[-7:]
+    shutil.copy(source_path_img, train_path + "/images")
+    shutil.copy(source_path_mask, train_path + "/masks")
+
